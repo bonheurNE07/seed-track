8ff6a6a3367b1e30d00d45ab5409789f9acb0c0b
bonheurNE07 <bonheurndezenc@gmail.com>
2025-06-23 23:22:17 +0200

    update: footer responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className="sticky bottom-0 z-50 w-full flex items-center justify-between px-6 py-3 sm:px-6 md:px-8 bg-white/30 dark:bg-[#171717]/97 backdrop-blur-md shadow-md transition-all duration-300">
+    <footer className="w-full flex items-center justify-between px-6 py-3 sm:px-6 md:px-8 bg-white/30 dark:bg-[#171717]/97 backdrop-blur-md shadow-md transition-all duration-300">
       {/* Left */}
       <div className="flex items-center space-x-2">
-        <span className="text-xs leading-tight max-w-screen-sm mx-auto text-center text-gray-500 dark:text-gray-400">
+        <span className="sm:block text-xs leading-tight max-w-screen-sm mx-auto text-center text-gray-500 dark:text-gray-400">
           © 2025 bn_e. All rights reserved.
         </span>
       </div>
@@ -13,7 +13,7 @@ const Footer: React.FC = () => {
       {/* Right */}
       <a
         href="mailto:bonheurndeze@gmail.com"
-        className="text-sm text-blue-600 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-500 text-center tracking-tight leading-tight"
+        className="hidden sm:block text-sm text-blue-600 hover:text-blue-400 dark:text-blue-400 dark:hover:text-blue-500 text-center tracking-tight leading-tight"
       >
         CONTACT THE DEVELOPER
       </a>
